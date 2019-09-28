45f301397679e38a74b116ec167b40c296cc6387
Huynh Nhu <huynhnhutran114@gmail.com>
2019-09-28 18:15:59 +0700

    Main

--- app/src/main/java/com/example/mycnscanner/MainActivity.java
+++ app/src/main/java/com/example/mycnscanner/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.mycnscanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img=(ImageView) findViewById(R.id.imglogo);
     }
 }
